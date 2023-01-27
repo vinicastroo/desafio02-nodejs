0e1e11ecddba922d38db7924dae655e8acc41a17
Vinicius Castro <viniciusc.d.c@hotmail.com>
2023-01-26 23:08:10 -0300

    adição extra nodejs prisma e rename

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,69 @@
+import { User } from "../../model/User";
+import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
+
+class UsersRepository implements IUsersRepository {
+  private users: User[];
+
+  private static INSTANCE: UsersRepository;
+
+  private constructor() {
+    this.users = [];
+  }
+
+  public static getInstance(): UsersRepository {
+    if (!UsersRepository.INSTANCE) {
+      UsersRepository.INSTANCE = new UsersRepository();
+    }
+
+    return UsersRepository.INSTANCE;
+  }
+
+  create({ name, email }: ICreateUserDTO): User {
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+    });
+
+    this.users.push(user);
+    return user;
+  }
+
+  findById(id: string): User | undefined {
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
+  }
+
+  findByEmail(email: string): User | undefined {
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
+  }
+
+  turnAdmin(receivedUser: User): User {
+    const filteredUsers = this.users.filter(
+      (user) => user.id !== receivedUser.id
+    );
+
+    const updatedUser = new User();
+    Object.assign(updatedUser, {
+      id: receivedUser.id,
+      name: receivedUser.name,
+      email: receivedUser.email,
+      admin: true,
+      created_at: receivedUser.created_at,
+    });
+
+    filteredUsers.push(updatedUser);
+    this.users = filteredUsers;
+    return updatedUser;
+  }
+
+  list(): User[] {
+    return this.users;
+  }
+}
+
+export { UsersRepository };
---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,27 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    if (!user.admin) {
+      throw new Error("You don't have permission");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
+  }
+}
+
+export { ListAllUsersUseCase };
